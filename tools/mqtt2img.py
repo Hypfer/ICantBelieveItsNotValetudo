5dc26e1f7b018df12f23082bc7d606be3a9d6281
Jérôme Labidurie <jerome@labidurie.fr>
2020-02-10 22:08:17 +0100

    Add simple python script to generate images from valetudo mqtt (#13)

---
+++ tools/mqtt2img.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PIL import Image, ImageDraw
+import paho.mqtt.subscribe as subscribe
+import json, argparse, os
+
+def json2img (data):
+	"""
+		Create vacuum image map from valetudo json data
+
+		:param data: json map data from valetudo mqtt
+		:return: the map as an image
+		:rtype: Image from PIL
+
+	"""
+	size= ( data['image']['dimensions']['width'], data['image']['dimensions']['height'])
+	left=data['image']['position']['left']
+	top=data['image']['position']['top']
+	#print (size)
+
+	img = Image.new('RGBA', size)
+
+	# draw floor & obstacles
+	draw = ImageDraw.Draw(img)
+	draw.point([ (x,y) for x,y in data['image']['pixels']["floor"] ],           fill=(  0, 118, 255))
+	draw.point([ (x,y) for x,y in data['image']['pixels']["obstacle_weak"] ],   fill=(102, 153, 255))
+	draw.point([ (x,y) for x,y in data['image']['pixels']["obstacle_strong"] ], fill=( 82, 174, 255))
+	del draw
+
+	# scale
+	resized = img.resize ( (size[0]*4, size[1]*4) )
+
+	# draw path
+	draw = ImageDraw.Draw(resized)
+	draw.line([ ((x/50-left)*4,(y/50-top)*4) for x,y in data['path']["points"] ], fill=(255,255,255))
+	del draw
+
+	# add charger & vacuum
+	path = os.path.dirname(os.path.realpath(__file__))
+	if data.has_key("charger"):
+		with Image.open("%s/img/charger.png"%(path)).convert('RGBA') as charger:
+			x = (data['charger'][0] / 50 - left) * 4
+			y = (data['charger'][1] / 50 - top) * 4
+			resized.paste ( charger, (x,y), charger )
+	with Image.open("%s/img/robot.png"%(path)).convert('RGBA') as robot:
+		x = (data['robot'][0] / 50 - left) * 4
+		y = (data['robot'][1] / 50 - top) * 4
+		resized.paste ( robot.rotate (-data['path']['current_angle']-90), (x,y), robot )
+
+	# crop to the map only
+	box = resized.getbbox()
+	cropped = resized.crop(box)
+
+	# replace alpha channel by solid color
+	bg = Image.new("RGBA", cropped.size, 'lightgrey')
+	return Image.alpha_composite(bg, cropped).convert('RGB')
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument("host", help="mqtt server (ie: valetudo broker_url)")
+	parser.add_argument("name", help="mqtt id (ie: valetudo identifier)")
+	parser.add_argument("-o", "--output", help="output image file", default="map.png")
+	parser.add_argument("-t", "--topic", help="topicPrefix", default="valetudo")
+	args = parser.parse_args()
+
+	message = subscribe.simple("%s/%s/map_data" % (args.topic, args.name), hostname=args.host)
+	data = json.JSONDecoder().decode(message.payload)
+
+	img = json2img (data)
+	img.save(args.output)
+
